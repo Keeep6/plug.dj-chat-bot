426cd30bf93510d489b935f3fee84871aa081572
Keeep6 <Jack.Nightingale911@gmail.com>
2013-03-26 01:15:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1582,11 +1582,11 @@
     fbCommand.prototype.functionality = function() {
       var m, msg;
       m = Math.floor(Math.random() * this.msgs.length);
-      msg = this.msgs[m].replace('{fb}', 'http://on.fb.me/HNzK5S');
+      msg = this.msgs[m].replace('{fb}', 'https://www.facebook.com/aBlueScholar5');
       return API.sendChat(msg);
     };
 
-    fbCommand.prototype.msgs = ["Don't have any friends in real life? That's ok, we'll be your friend.  Join our facebook group: {fb}", "Wondering what TIMarbury looks like?  Join our facebook group ({fb}) and find out for yourself!", "We have a facebook group. Join it. Please. {fb}", "The Dubstep Den is now on friendster! lol just kidding.  Here's our facebook group: {fb} you should join.", "I bet you're handsome.  Join our facebook group so me0w can stalk your photos: {fb}"];
+    fbCommand.prototype.msgs = ["If you have any questions, suggestions, or just simply want to talk.  Message me on facebook!: {fb}";
 
     return fbCommand;
 
