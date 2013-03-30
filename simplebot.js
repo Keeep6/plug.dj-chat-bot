cb8637f19c7f56f52a1eb4c71aab43409d496126
Keeep6 <Jack.Nightingale911@gmail.com>
2013-03-30 03:41:43 +0000

    Update simplebot.js

--- simplebot.js
+++ simplebot.js
@@ -859,8 +859,8 @@
       var msg, name, r, user;
       msg = this.msgData.message;
       r = new RoomHelper();
-      if (msg.length > 10) {
-        name = msg.substr(10);
+      if (msg.length > 11) {
+        name = msg.substr(11);
         user = r.lookupUser(name);
         if (user === false) {
           API.sendChat("/me punishes @" + this.msgData.from + " for getting the syntax wrong.");
